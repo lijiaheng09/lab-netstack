730d0119b7d2442c9af6c7f25d5d21435806ac37
Jiaheng Li <lijiaheng09@outlook.com>
2022-11-05 01:11:38 -0700

    make-handin for Lab 2

--- make-handin.sh
+++ make-handin.sh
@@ -1,7 +1,7 @@
-LAB=1
+LAB=2
 
 SUF=`cat handin-suf.txt`
 mkdir -p .tmp/lab"$LAB"
-cp -r src/ CMakeLists.txt report/README.pdf report/not-implemented.pdf checkpoints/ .tmp/lab"$LAB"
+cp -r src/ CMakeLists.txt report/README.pdf report/not-implemented.pdf checkpoints/ utils/ .tmp/lab"$LAB"
 tar -cf lab"$LAB"-"$SUF".tar -C .tmp lab"$LAB"
 rm -r .tmp/
